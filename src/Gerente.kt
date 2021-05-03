77c9614483f0efce73ada5a126be907bc785e5c8
Denis <dineresc@gmail.com>
2021-05-03 18:52:09 -0300

    open funcionario class

--- src/Gerente.kt
+++ src/Gerente.kt
@@ -1,13 +1,17 @@
 class Gerente(
-    val nome: String,
-    val cpf: String,
-    val salario: Double,
+    nome: String,
+    cpf: String,
+    salario: Double,
     val senha: Int
+) : Funcionario(
+    nome = nome,
+    cpf = cpf,
+    salario = salario
 ) {
 
-    fun bonificacao(): Double {
+/*    fun bonificacao(): Double {
         return salario * 0.2
-    }
+    }*/
 
     fun autentica(senha: Int): Boolean {
         if(this.senha == senha){
